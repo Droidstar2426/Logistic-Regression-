de5b448e2e664f7f77d964de62272f84d98002eb
Paras Jain <paras.2426@gmail.com>
2019-08-14 18:53:20 +0530

    Update logistic.m

--- logistic.m
+++ logistic.m
@@ -1,7 +1,7 @@
-x=load('diabetes.txt');
+x=load('traindata.txt'); %training data
 x1=x(:,1:end-1);
 y=x(:,end);
-%normalization=true;
+%normalization=true; %normalization if necessary
 %if(normalization)
    % for i=1:size(x1,2)
   %      x1(:,i)=(x1(:,i)-max(x1(:,i)))/(max(x1(:,i))-min(x1(:,i)));
@@ -9,7 +9,6 @@ y=x(:,end);
 %end
 iterations=4000;
 alpha=0.000001;
-%alpha=0.0000000000000000000003;
 x1=[ones(size(y,1),1) x1];
 
 theta=zeros(size(x1,2),1);
@@ -18,8 +17,8 @@ theta=zeros(size(x1,2),1);
 %y2=y1(:,1:3);
 y2=[1,4,109,64,44,99,34.8,0.905,26];
 accuracy=sigmoidfunc(y2,theta);
-for i=1:size(accuracy,1)
-if (accuracy(i)>0.49)
+for i=1:size(accuracy,1)  %prediction for binary output
+if (accuracy(i)>0.49)                
     accuracy(i)=1
 else
     accuracy(i)=0
